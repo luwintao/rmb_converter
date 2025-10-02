37e4414bb3f3083feef431266573635126e9af8d
luwintao <luwentao@gmail.com>
2025-10-02 09:51:13 +0800

    1
    1

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo 正在安装依赖...
-pip install -r requirements.txt
-
-echo 正在打包程序...
-pyinstaller --onefile --windowed --name "RMBConverter" --icon=NONE rmb_converter.py
-
-echo 打包完成！可执行文件在 dist 文件夹中
-pause
--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "正在安装依赖..."
-pip install -r requirements.txt
-
-echo "正在打包程序..."
-pyinstaller --onefile --windowed --name "RMBConverter" --icon=NONE rmb_converter.py
-
-echo "打包完成！可执行文件在 dist 文件夹中"
